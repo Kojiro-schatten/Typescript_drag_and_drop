f22a93ae683eb25d9a24d4a70fbc4c20db0673de
Kojiro-schatten <kouschatten@gmail.com>
2020-04-15 01:54:04 +0900

    added validation

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,45 @@
+//Validation 文字数制限
+interface Validatable {
+  value: string | number;
+  // ? = value or undefined. => 無くても良い。
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
 
+function validate(validatableInput: Validatable) {
+  let isValid = true;
+  if (validatableInput.required) {
+    // typeGuardが無いと、Validatableは常にstringを返すわけでは無いのでtrimエラーとなる
+    // 今回は、toStringを使ってtypeGuardは実装していない。
+    if (validatableInput.required){
+      isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+    }
+    // Description
+    if (
+      validatableInput.minLength != null && 
+      typeof validatableInput.value === 'string'
+      ) {
+      isValid = isValid && validatableInput.value.length > validatableInput.minLength;
+    }
+    if (
+      validatableInput.maxLength != null && 
+      typeof validatableInput.value === 'string'
+      ) {
+      isValid = isValid && validatableInput.value.length < validatableInput.maxLength;
+    }
+  }
+  // People
+  if (validatableInput.min != null && typeof validatableInput.value === 'number') {
+    isValid = isValid && validatableInput.value > validatableInput.min;
+  }
+  if (validatableInput.max != null && typeof validatableInput.value === 'number') {
+    isValid = isValid && validatableInput.value < validatableInput.max;
+  }
+  return isValid;
+}
 // autobind decorator
 // _, _2で使わないが必要なものとして認識。
 function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
@@ -25,7 +66,7 @@ class ProjectInput {
   element: HTMLFormElement;
   titleInputElement: HTMLInputElement;
   descriptionInputElement: HTMLInputElement;
-  inputInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
 
   constructor() {
@@ -44,16 +85,65 @@ class ProjectInput {
 
     this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
     this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
-    this.inputInputElement = this.element.querySelector('#people') as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
 
     this.configure();
     this.attach();
   }
 
+  private gatherUserInput(): [string, string, number] | void {
+    // [string, string, number] は、validate指定。
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    const titleValidatable: Validatable = {
+      value: enteredTitle,
+      required: true
+    };
+
+    const descriptionValidatable: Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5
+    };
+
+    const peopleValidatable: Validatable = {
+      value: +enteredPeople,
+      required: true,
+      min: 1,
+      max: 5
+    };
+
+    if(
+      // 3つのうち1つでもエラーがあればalertが発生する。
+      !validate(titleValidatable) ||
+      !validate(descriptionValidatable) ||
+      !validate(peopleValidatable) 
+    ) {
+      alert('Invalid input, please try again')
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople];
+    }
+  }
+
+  private clearInputs() {
+    this.titleInputElement.value = '';
+    this.descriptionInputElement.value = '';
+    this.peopleInputElement.value = '';
+  }
+
   @Autobind
   private submitHandler(event: Event) {
+    // 即時実行を阻止する。
     event.preventDefault();
-    console.log(this.titleInputElement.value);
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      console.log(title, desc, people);
+      this.clearInputs();
+    }
   }
 
   private configure() {
